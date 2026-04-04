dbf52af7c4e92d674dced91d8113f44a9c7333f2
Yaryna <yarynka.hoy@gmail.com>
2026-04-04 20:15:45 +0300

    add queue.py

---
+++ Implement_Queue_using_Stacks/queue.py
@@ -0,0 +1,48 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, x):
+        self.items.append(x)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def empty(self):
+        return len(self.items) == 0
+
+
+class MyQueue:
+
+    def __init__(self):
+        self.in_q = Stack()
+        self.out_q = Stack()
+
+    def push(self, x: int) -> None:
+        self.in_q.push(x)
+
+    def pop(self) -> int:
+        if self.out_q.empty():
+            while not self.in_q.empty():
+                self.out_q.push(self.in_q.pop())
+        return self.out_q.pop()
+
+    def peek(self) -> int:
+        if self.out_q.empty():
+            while not self.in_q.empty():
+                self.out_q.push(self.in_q.pop())
+        return self.out_q.peek()
+
+    def empty(self) -> bool:
+        return self.in_q.empty() and self.out_q.empty()
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
