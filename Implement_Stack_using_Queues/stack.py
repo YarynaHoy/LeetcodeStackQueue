17529315b3cfdf6954027bcbd948630462e2384d
Yaryna <yarynka.hoy@gmail.com>
2026-04-06 12:06:18 +0300

    remaking stack.py with linked list

--- Implement_Stack_using_Queues/stack.py
+++ Implement_Stack_using_Queues/stack.py
@@ -1,18 +1,51 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
 class Queue:
     def __init__(self):
-        self.items = []
+        self.head = None
+        self.size = 0
+
     def push(self, x: int) -> None:
-        self.items.append(x)
+        new_node = Node(x)
+        new_node.next = self.head
+        self.head = new_node
+        self.size += 1
 
 
     def pop(self) -> int:
-        return self.items.pop(0)
+        if not self.head:
+            return None
+
+        if self.head.next is None:
+            val = self.head.value
+            self.head = None
+            self.size -= 1
+            return val
+
+        cur = self.head
+        while cur.next.next:
+            cur = cur.next
+
+        val = cur.next.value
+        cur.next = None
+        self.size -= 1
+        return val
+
 
     def peek(self) -> int:
-        return self.items[0]
+        if not self.head:
+            return None
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        return cur.value
 
     def empty(self) -> bool:
-        return len(self.items) == 0
+        return self.head is None
+
 
 class MyStack:
 
